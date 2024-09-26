80612eb7cc996024d750838453ed97c471fea726
Edwin <oriokie@icloud.com>
2024-09-26 09:49:19 +0300

    ✅ created the utils folder and the redis.js file that contains the RedisClient class for initializing the redis client and checking the state of the redis server.

---
+++ utils/redis.js
@@ -0,0 +1,67 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+/**
+ * RedisClient Class
+ * provides asyncrounous methods (get, set, del)
+ * Also checks if the client is connected to the server
+ */
+class RedisClient {
+  /**
+   * Constructor for initializing the RedisClient
+   * Also sets up promisified methods
+   */
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client error: ${error}`);
+    });
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  /**
+   * A function that checks if the client is connected to the server
+   * @returns {boolean} true if connected, false otherwise
+   */
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * A function that gets the value of a key from the Redis server
+   * @param {string} key the key
+   * @returns {string} the value of the key
+   */
+  async get(key) {
+    const value = await this.getAsync(key);
+    return value;
+  }
+
+  /**
+   * A function that sets a key value pair in the Redis server
+   * @param {string} key the key
+   * @param {string} value the value
+   * @param {number} duration the expiration time for the key
+   * @returns {string} the value of the key
+   */
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  /**
+   * A function that deletes a key from the Redis server
+   * @param {string} key the key
+   */
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
