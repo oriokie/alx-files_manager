6b4e227c0814a2dbdd53540c831b9ec6bfe18791
Edwin <oriokie@icloud.com>
2024-09-28 22:53:50 +0300

    edited

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,45 +3,46 @@ const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 const { ObjectID } = require('mongodb');
 
+exports.postNew = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
 
-exports.postNew = async(req, res) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
+    const database = dbClient.client.db(dbClient.database);
+    const collection = database.collection('users');
 
-        const database = dbClient.client.db(dbClient.database);
-        const collection = database.collection('users');
-
-        if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
-        }
-
-        if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-        }
-
-        const user = await collection.findOne({ email: email });
-        if (user) {
-            return res.status(400).json({ error: 'Already exist' });
-        }
-
-        const hashedPassword = sha1(password);
-        const newUser = await collection.insertOne({ email: email, password: hashedPassword });
-        return res.status(201).json({ id: newUser.insertedId, email: email });
-
-    } catch (error) {
-        console.error(error);
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
     }
-}
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const user = await collection.findOne({ email: email });
+    if (user) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const newUser = await collection.insertOne({ email: email, password: hashedPassword });
+    return res.status(201).json({ id: newUser.insertedId, email: email });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 exports.getMe = async (req, res) => {
-    const token = req.get('X-Token');
+  const token = req.get('X-Token');
 
-    const userId = await redisClient.get(`auth_${token}`);
+  const userId = await redisClient.get(`auth_${token}`);
 
-    const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ _id: ObjectID(userId) });
-    if (!user) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    return res.status(200).json({ id: user._id.toString(), email: user.email });
+  const user = await dbClient.client
+    .db(dbClient.database)
+    .collection('users')
+    .findOne({ _id: ObjectID(userId) });
+  if (!user) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  return res.status(200).json({ id: user._id.toString(), email: user.email });
 };
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes')
+const routes = require('./routes');
 
 const app = express();
 const port = process.env.port;
@@ -8,5 +8,5 @@ app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
