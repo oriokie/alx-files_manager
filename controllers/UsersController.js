933c3f4f6c690cc65eef2486bd833f6e2324bec2
Edwin <oriokie@icloud.com>
2024-09-29 21:46:51 +0300

    added some files to fix the issue

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,47 +1,54 @@
 const sha1 = require('sha1');
 const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
-const { ObjectID } = require('mongodb');
+const { ObjectId } = require('mongodb');
 
+exports.postNew = async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
 
-exports.postNew = async(req, res) => {
-    try {
-        const email = req.body.email;
-        const password = req.body.password;
-
-        const database = dbClient.client.db(dbClient.database);
-        const collection = database.collection('users');
-
-        if (!email) {
-        return res.status(400).json({ error: 'Missing email' });
-        }
-
-        if (!password) {
-        return res.status(400).json({ error: 'Missing password' });
-        }
-
-        const user = await collection.findOne({ email: email });
-        if (user) {
-            return res.status(400).json({ error: 'Already exist' });
-        }
-
-        const hashedPassword = sha1(password);
-        const newUser = await collection.insertOne({ email: email, password: hashedPassword });
-        return res.status(201).json({ id: newUser.insertedId, email: email });
-
-    } catch (error) {
-        console.error(error);
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
     }
-}
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingUser = await dbClient.usersCollection.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = sha1(password);
+    const newUser = await dbClient.usersCollection.insertOne({
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      id: newUser.insertedId.toString(),
+      email,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 
 exports.getMe = async (req, res) => {
-    const token = req.get('X-Token');
+  try {
+    // The userId is now available on the request object thanks to the middleware
+    const getuserId = req.userId;
 
-    const userId = await redisClient.get(`auth_${token}`);
-
-    const user = await dbClient.client.db(dbClient.database).collection('users').findOne({ _id: ObjectID(userId) });
+    const user = await dbClient.usersCollection.findOne({ _id: new ObjectId(getuserId) });
     if (!user) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
+
     return res.status(200).json({ id: user._id.toString(), email: user.email });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 };
